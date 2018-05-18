9e25a08dfe6360c628affb2b19855ed0a2f1a7a7
Hung Tran <hungtantran@gmail.com>
2018-05-18 00:37:38 -0700

    Working somewhat

--- lab10/planning.py
+++ lab10/planning.py
@@ -10,6 +10,9 @@ import cozmo
 import time
 from cozmo.util import degrees, Angle, Pose, distance_mm, speed_mmps
 
+CUBE_SIZE = 50.0
+ROBOT_WIDTH = 50.0
+ROBOT_LENGTH = 75.0
 
 def astar(grid, heuristic):
     """Perform the A* search algorithm on a defined grid
@@ -118,59 +121,96 @@ def heuristic(current, goal):
 
 def xyToCoor(grid, start, startRobot, x, y):
     return (
-        int((x - startRobot[0])/grid.scale + start[0]) + 1,
-        int((y - startRobot[1])/grid.scale + start[1]) + 1
+        int(float(x - startRobot[0])/grid.scale + start[0]),
+        int(float(y - startRobot[1])/grid.scale + start[1])
     )
 
 def coorToXY(grid, start, startRobot, coorX, coorY):
     return (
-        (coorX + 1 -start[0]) * grid.scale + startRobot[0],
-        (coorY + 1 -start[1]) * grid.scale + startRobot[1]
+        (coorX - start[0]) * grid.scale + startRobot[0] + 25,
+        (coorY - start[1]) * grid.scale + startRobot[1] + 25
     )
 
-def drive_from_to(grid, start, startRobot, robot, x, y):
-    x1 = robot.pose.position.x
-    y1 = robot.pose.position.y
+def lightCubeXYToBorderCoors(grid, start, startRobot, x, y):
+    offset = [-50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50]
+    cornersXY = []
+    for i in [-50, 50]:
+        for off in offset:
+            cornersXY.append((x + i, y + off))
+    for i in [-50, 50]:
+        for off in offset:
+            cornersXY.append((x + off, y + i))
+
+    cornersCoors = []
+    for xy in cornersXY:
+        cornersCoors.append(xyToCoor(grid, start, startRobot, xy[0], xy[1]))
+    return cornersCoors
+
+def turn_to(grid, start, startRobot, robot, coorX, coorY):
+    x, y = coorToXY(grid, start, startRobot, coorX, coorY)
+
+    x1, y1 =front_robot(robot)
     
     angle1 = robot.pose.rotation.angle_z.degrees
     desired_angle = math.degrees(math.atan(float(y - y1) / float(x - x1)))
     turn_angle = desired_angle - angle1
     robot.turn_in_place(degrees(turn_angle)).wait_for_completed()
+    if abs(turn_angle) > 20:
+        return True
+    return False
 
-    dist = math.sqrt(math.pow(x - x1, 2) + math.pow(y - y1, 2)) - 50
+def drive_straight_to(grid, start, startRobot, robot, coorX, coorY):
+    print("Drive straight")
+    x, y = coorToXY(grid, start, startRobot, coorX, coorY)
+
+    x1, y1 = front_robot(robot)
+    dist = math.sqrt(math.pow(x - x1, 2) + math.pow(y - y1, 2))
     action = robot.drive_straight(distance_mm(dist), speed_mmps(15))
     
     while not action.is_completed:
-        x1 = robot.pose.position.x
-        y1 = robot.pose.position.y
+        x1, y1 = front_robot(robot)
         newStart = xyToCoor(grid, start, startRobot, x1, y1)
         if grid.getStart() != newStart:
             grid.clearStart()
             grid.setStart(newStart)
+    print("Drive straight done")
 
 
-def drive_toward_cube(grid, start, startRobot, robot, cube):
-    drive_from_to(grid, start, startRobot, robot, cube.pose.position.x, cube.pose.position.y)
+def update_obstacles(grid, start, startRobot, robot):
+    objects = robot.world.visible_objects
+    for obj in objects:
+        if obj.cube_id != cozmo.objects.LightCube1Id:
+            cornersCoors = lightCubeXYToBorderCoors(grid, start, startRobot, obj.pose.position.x, obj.pose.position.y)
+            for corner in cornersCoors:
+                grid.addObstacle((corner[0], corner[1]))
 
 
-def drive_to_center(grid, start, startRobot, robot):
-    x, y = coorToXY(grid, start, startRobot, grid.width/2, grid.height/2)
-    drive_from_to(grid, start, startRobot, robot, x, y)
+def front_robot(robot):
+    x = robot.pose.position.x
+    y = robot.pose.position.y
+    #angle = robot.pose.rotation.angle_z.degrees
+    #return (x + math.cos(math.radians(angle)) * 40, y + math.sin(math.radians(angle)) * 40)
+    return (x, y)
 
 
-def findCube(grid, startRobot, robot: cozmo.robot.Robot):
+def findCube(grid, robot: cozmo.robot.Robot):
+    startRobot = front_robot(robot)
     start = grid.getStart()
-
     robot.set_head_angle(degrees(0)).wait_for_completed()
 
     grid.clearGoals()
     grid.addGoal((grid.width/2, grid.height/2))
-    found = False
+    goal = None
+    angle = None
+    reach_center = False
     while True:
         print()
         print("1. Another loop")
-        rCoorX, rCoorY = xyToCoor(grid, start, startRobot, robot.pose.position.x, robot.pose.position.y)
-        reach_center = (rCoorX == grid.width/2) and (rCoorY == grid.height/2)
+        curRobot = front_robot(robot)
+        rCoorX, rCoorY = xyToCoor(grid, start, startRobot, curRobot[0], curRobot[1])
+        reach_center = reach_center or (
+            (rCoorX == grid.width/2 or rCoorX == grid.width/2 + 1 or rCoorX == grid.width/2 - 1) and
+            (rCoorY == grid.height/2 or rCoorY == grid.height/2 + 1 or rCoorY == grid.height/2 -1))
         print("reach_center = ", reach_center)
         grid.clearStart()
         grid.setStart((rCoorX, rCoorY))
@@ -178,33 +218,63 @@ def findCube(grid, startRobot, robot: cozmo.robot.Robot):
         try:
             # Update visible objects
             print("2. Update objects")
-            objects = robot.world.visible_objects
-            grid.clearObstacles()
-            for obj in objects:
-                if obj.cube_id == cozmo.objects.LightCube1Id:
-                    goalCoorX, goalCoorY = xyToCoor(grid, start, startRobot, obj.pose.position.x, obj.pose.position.y)
-                    grid.clearGoals()
-                    grid.addGoal((goalCoorX, goalCoorY))
-                    found = True
-                else:
-                    objCoorX, objCoorY = xyToCoor(grid, start, startRobot, obj.pose.position.x, obj.pose.position.y)
-                    print(obj)
-                    print(robot.pose)
-                    print(objCoorX, objCoorY)
-                    grid.addObstacle((objCoorX, objCoorY))
+            update_obstacles(grid, start, startRobot, robot)
 
             # Find the goal (cube1), update the grid and drive toward it
-            if found:
-                print("3. Found goals")
-                drive_toward_cube(grid, start, startRobot, robot)
-                return
+            if goal:
+                print("8. Found goals")
+                update_obstacles(grid, start, startRobot, robot)
+                grid.clearPath()
+                print("9. Finding path")
+                astar(grid, heuristic)
+
+                path = grid.getPath()
+                print("10. Found path ", path)
+                if grid.checkPath() and len(path) > 2:
+                    if turn_to(grid, start, startRobot, robot, path[1][0], path[1][1]):
+                        continue
+                    drive_straight_to(grid, start, startRobot, robot, path[1][0], path[1][1])
+                # Reach goal:
+                if len(path) <= 2:
+                    turn_to(grid, start, startRobot, robot, goal[0], goal[1])
+                    stopevent.set()
+                    return
+                continue
+
+            # Found the goal (cube1)
+            objects = robot.world.visible_objects
+            for obj in objects:
+                if obj.cube_id == cozmo.objects.LightCube1Id:
+
+                    cornersCoors = lightCubeXYToBorderCoors(grid, start, startRobot, obj.pose.position.x, obj.pose.position.y)
+                    for corner in cornersCoors:
+                        grid.addObstacle((corner[0], corner[1]))
+
+                    coorX, coorY = xyToCoor(grid, start, startRobot, obj.pose.position.x, obj.pose.position.y)
+                    angle = obj.pose.rotation.angle_z.degrees
+                    if angle < 45 and angle > -45:
+                        coorX -= 3
+                    elif angle > 45 and angle < 135:
+                        coorY -= 3
+                    elif angle > 135 or angle < -135:
+                        coorX += 3
+                    else:
+                        coorY += 3
+                    grid.clearGoals()
+                    goal = (coorX, coorY)
+                    grid.addGoal((coorX, coorY))
+                    continue
 
             if reach_center:
                 print("4. Reach center")
+                grid.clearPath()
+                grid.clearGoals()
                 # Once reach center, keep turning in place
                 robot.turn_in_place(degrees(20)).wait_for_completed()
             else:
                 print("5. Drive toward center")
+
+                update_obstacles(grid, start, startRobot, robot)
                 # If not reach center, try to reach center
                 # Update current robot position as start and recalculate path to center and drive toward it, 1 cell at a time
                 grid.clearPath()
@@ -214,12 +284,13 @@ def findCube(grid, startRobot, robot: cozmo.robot.Robot):
                 path = grid.getPath()
                 print("7. Found path ", path)
                 if grid.checkPath() and len(path) > 1:
-                    x, y = coorToXY(grid, start, startRobot, path[1][0], path[1][1])
-                    drive_from_to(grid, start, startRobot, robot, x, y)
+                    if turn_to(grid, start, startRobot, robot, path[1][0], path[1][1]):
+                        continue
+                    drive_straight_to(grid, start, startRobot, robot, path[1][0], path[1][1])
         except Exception as e:
             print("Exception")
             print(e)
-            pass
+            return
 
 def cozmoBehavior(robot: cozmo.robot.Robot):
     """Cozmo search behavior. See assignment description for details
@@ -235,10 +306,8 @@ def cozmoBehavior(robot: cozmo.robot.Robot):
         
     global grid, stopevent
     
-    x1 = robot.pose.position.x
-    y1 = robot.pose.position.y
     while not stopevent.is_set():
-        findCube(grid, (x1, y1), robot)
+        findCube(grid, robot)
 
 ######################## DO NOT MODIFY CODE BELOW THIS LINE ####################################
 
